610734894c14a241488b125008c3d154a45df4e4
VovaSakai <vovas2345@gmail.com>
2017-03-07 08:57:45 +1000

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
-"use strict";
-
-jQuery(document).ready(function() {
-
- //Slick
- jQuery('.slider').slick();
-
+//Anchor
+$("body").on('click', '[href*="#"]', function(e){
+  var fixed_offset = 0;
+  $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+  e.preventDefault();
 });
