328e7a4cc770b9d50c020086ce9b914979716ac8
VovaSakai <vovas2345@gmail.com>
2017-03-16 07:31:41 +1000

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+//Touch-menu
+$(document).ready(function(){
+			var touch = $('#touch-menu');
+		    var menu = $('.nav');
+
+		    $(touch).on('click', function(e) {
+		        e.preventDefault();
+		        menu.slideToggle();
+		    });
+		    $(window).resize(function(){
+		        var wid = $(window).width();
+		        if(wid > 760 && menu.is(':hidden')) {
+		            menu.removeAttr('style');
+		        }
+		    });
+
+		});
 //Anchor
 $("body").on('click', '[href*="#"]', function(e){
   var fixed_offset = 0;
